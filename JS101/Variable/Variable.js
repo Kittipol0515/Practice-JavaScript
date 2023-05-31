2987b2d8d496762232681aeec6e6e37c136e55ae
kittipol <kittipol.surapinchai@gmail.com>
2023-05-31 16:04:46 +0700

    JS 101

--- JS101/Variable/Variable.js
+++ JS101/Variable/Variable.js
@@ -1,4 +1,5 @@
 let myName = "Kittipol"
+myName = "KIttipolX"
 
 let myAge = 20
 
@@ -8,10 +9,12 @@ const before = "Nomain"
 
 const after = "JSD4 Kititpol"
 
+const vat = 0.08;
+
 console.log("Before: " + before)
 console.log("After: " + after)
 
 console.log(myName)
 console.log(myAge)
 console.log(myName + " " + myAge)
-console.log(`My name is ${myName} and my age is ${myAge}, want to play ${game}`)
+console.log(`My name is ${myName} and my age is ${myAge}, want to play ${game} and need to pay ${vat}`)
