c2dd3e8fde6678d1dd3d871f0390760d1724dec3
kittipol <kittipol.surapinchai@gmail.com>
2023-05-31 21:37:31 +0700

    JS101

---
+++ JS101/Variable/DataTypes.js
@@ -0,0 +1,39 @@
+let x = 10;
+let y = 20;
+
+let money = 2000;
+
+let price = 99.99.toString(); 
+
+let message = "Nomian"
+let myName = "Kitti"
+
+let checkName = false;
+
+let age = parseInt("20.15") // เปลี่ยนแปลงค่าข้อมูล parseInt,parseFloat,+ string to number
+
+
+console.log(money)
+console.log(price)
+console.log(message)
+console.log(myName)
+console.log(checkName)
+console.log(age)
+
+console.log(typeof(message))
+console.log(typeof(money))
+console.log(typeof(myName))
+console.log(typeof(price))
+console.log(typeof(checkName))
+console.log(typeof(age))
+
+
+console.log(x,y);
+console.log(x+y);
+
+// let age = 99.99.toString()
+
+// console.log(age)
+// console.log(typeof(age))
+
+
