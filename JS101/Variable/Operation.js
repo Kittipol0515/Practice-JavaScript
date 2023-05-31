59be6289c0880af62eb397d0d81ac6f5c9f40022
kittipol <kittipol.surapinchai@gmail.com>
2023-05-31 23:46:34 +0700

    JS101

---
+++ JS101/Variable/Operation.js
@@ -0,0 +1,23 @@
+let x = 5
+let y = 5
+let a = 10
+let b = 13
+
+// console.log(x+y) // plus
+// console.log(a-y) // minus
+// console.log(a*b) // multiply
+// console.log(a**x) // power of
+// console.log(a/x) // divide
+// console.log(b%y) // remainder
+
+
+console.log(a !== b)
+console.log(x == y)
+console.log(x > y)
+
+//compound assignment
+// x+=y = 10
+// x*=y = 25
+// x/=y = 1
+// x %= y = 0
+console.log(x)
