07b56955ec16b97d49119d80ff5ad472c78ac263
kittipol <kittipol.surapinchai@gmail.com>
2023-05-31 15:56:40 +0700

    JS101

--- JS101/Variable/Variable.js
+++ JS101/Variable/Variable.js
@@ -2,7 +2,16 @@ let myName = "Kittipol"
 
 let myAge = 20
 
+const game = "PS5"
+
+const before = "Nomain"
+
+const after = "JSD4 Kititpol"
+
+console.log("Before: " + before)
+console.log("After: " + after)
 
 console.log(myName)
 console.log(myAge)
 console.log(myName + " " + myAge)
+console.log(`My name is ${myName} and my age is ${myAge}, want to play ${game}`)
