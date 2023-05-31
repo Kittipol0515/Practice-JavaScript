b163da6217704aede37c963f6c53d1dab45ae844
kittipol <kittipol.surapinchai@gmail.com>
2023-05-31 14:46:32 +0700

    JS101

---
+++ JS101/index.js
@@ -0,0 +1,8 @@
+ document.write("<h1>Kittipol</h1>")
+ document.write("<h2>Basic javascript</h2>")
+ document.write("<p>lorem iokHASdkjsbhf</p>")
+
+ alert("Hello wood")
+ console.log("Hellowood")
+ console.err("Virus boom")
+ console.warn("Junkyard")
