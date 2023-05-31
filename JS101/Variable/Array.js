ed9cc4e498e5000d143dc4e51db922db1aefcda1
kittipol <kittipol.surapinchai@gmail.com>
2023-05-31 22:45:59 +0700

    JS101

---
+++ JS101/Variable/Array.js
@@ -0,0 +1,32 @@
+// let number1 = 1
+// let number2 = 2
+// let number3 = 3
+// let number4 = 4
+// let number5 = 5
+// let number6 = 6
+
+let number = [0,1,2,3,4,5,6]
+let color = ['red','blue','green','yellow','white','black']
+
+
+console.log(color)
+color[1] = "whitesmoke"
+console.log(color[1]) // Find Index
+console.log(color.length)// Find length
+
+// operation
+
+let x = 5
+let y = 5
+let a = 10
+let b = 13
+
+console.log(x+y) // plus
+console.log(a-y) // minus
+console.log(a*b) // multiply
+console.log(a**x) // power of
+console.log(a/x) // divide
+console.log(b%y) // remainder
+
+
+
