43b998fd2a785d19bc460ae0783f33e8d1802930
kittipol <kittipol.surapinchai@gmail.com>
2023-06-01 15:26:34 +0700

    JS101

--- JS101/Variable/Condition.js
+++ JS101/Variable/Condition.js
@@ -1,8 +1,51 @@
-age = 20
+// age = 20
 
-if (age>18) {
-    console.log(true)
+// if (age >=18 || age <=14) {
+//     console.log(true)
+// } else {
+//     console.log(false)
+// }
+
+// let balance = 1000
+// let withDraw = 500
+
+// if (withDraw<=balance) {
+//     console.log("Deposit: " + withDraw)
+//     balance = balance-withDraw
+//     console.log("Balance: " + balance)
+// } else {
+//     console.log("You so dumped")
+// }
+
+let score = 80
+
+if (score >= 70) {
+    console.log("grade A ")
+}else if (score > 60) {
+    console.log("grade B ")
+}else if (score > 50) {
+    console.log("grade C ")
 } else {
-    console.log(false)
+    console.log("Failure ")
 }
 
+// Ternary Operator 
+// pass = score>70 ? "pass" : "fail"
+
+// console.log(pass)
+
+let age = 20 
+
+if (age <= 15) {
+    if(age == 15) {
+        console.log("Grade 9")
+    } else if (age == 14) {
+        console.log("Grade8")
+    } else if (age == 13) {
+        console.log("Grade 7")
+    } else {
+        console.log("Kindergarden")
+    }
+} else {
+    console.log("Goodie")
+}
